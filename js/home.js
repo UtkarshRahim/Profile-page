55cfd69a56a85af0fad6711cb72581be361c679f
utkarshrahim <utkarshrahim@gmail.com>
2019-10-15 15:00:22 +0530

    Makeover and made login the home page

---
+++ js/home.js
@@ -0,0 +1,38 @@
+"use strict";
+var alertValue = null;
+$('.message a').click(function () {
+   $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+});
+
+$(document).ready(function () {
+   $('#login-button').click(function () {
+
+      var username = $('#username').val();
+      var password = $('#password').val();
+
+      if (username === "admin" && password === "password") {
+
+       location.href="./../pages/home.html";
+     
+      }
+      else {
+         $("#alert-box").html("<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> <a href=\"#!\" class=\"alert-link\">Incorrect username or password.</div>");
+         alertValue = "Incorrect";
+      }
+
+      return false;
+     
+   });
+
+   
+   $('#username').focus(function () {
+      if (alertValue === "Incorrect")
+         $("#alert-box").html("");
+   });
+   $('#password').focus(function () {
+      if (alertValue === "Incorrect")
+         $("#alert-box").html("");
+   });
+
+
+});
