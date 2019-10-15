27493ca6cb4031f671b9ef7917363958f778065c
Utkarsh Rahim <utkarsh.rahim@bridgesgi.com>
2019-10-15 12:16:00 +0530

    Adding a login condition and link

--- js/login.js
+++ js/login.js
@@ -6,6 +6,12 @@ $('.message a').click(function () {
 
 $(document).ready(function () {
    $('#login-button').click(function () {
+      
+      var username = $('#username').val();
+      var password = $('#password').val();
       alert("Sabar kar bhai, screen ka pehla din hai");
+     if(username==="admin" && password==="password")
+         location.href = "https://neeldore.github.io";
+         return false;
    });
 });
