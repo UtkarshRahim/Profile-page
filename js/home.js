306881095439813fa99f8aadb5a336da5ac1c48c
utkarshrahim <utkarshrahim@gmail.com>
2019-10-15 15:15:50 +0530

    Wishing mansi a good day

---
+++ js/home.js
@@ -0,0 +1,7 @@
+"use strict";
+$(document).ready(function () {
+$('#button').click(function () {
+    $('#alert').html("<div class=\"alert alert-primary\" role=\"alert\"><strong>Hehe !</strong> It's still under construction na. That's why only these small things added. BTW, Everything will be fine. If yuo need me, I am a ping away and you do look great.</div>");
+    return false;
+});
+});
