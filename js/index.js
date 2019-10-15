992344a8e47c3aa429ee5eae4c8a8cfbe6dd9d93
utkarshrahim <utkarshrahim@gmail.com>
2019-10-15 17:56:26 +0530

    Added loading to index and added skills and academics tab

--- js/index.js
+++ js/index.js
@@ -1,38 +1,9 @@
 "use strict";
-var alertValue = null;
-$('.message a').click(function () {
-   $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
-});
+
 
 $(document).ready(function () {
-   $('#login-button').click(function () {
-
-      var username = $('#username').val();
-      var password = $('#password').val();
-
-      if (username === "admin" && password === "password") {
-
-       location.href="./../pages/home.html";
-     
-      }
-      else {
-         $("#alert-box").html("<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> <a href=\"#!\" class=\"alert-link\">Incorrect username or password.</div>");
-         alertValue = "Incorrect";
-      }
-
-      return false;
-     
-   });
-
+   setTimeout(function(){
+      location.href="./pages/home.html";
+   },2000);
    
-   $('#username').focus(function () {
-      if (alertValue === "Incorrect")
-         $("#alert-box").html("");
-   });
-   $('#password').focus(function () {
-      if (alertValue === "Incorrect")
-         $("#alert-box").html("");
-   });
-
-
 });
