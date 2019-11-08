824ba928a16b43505b854f759e68c0e12db8f747
utkarshrahim <utkarshrahim@gmail.com>
2019-11-09 03:13:05 +0530

    Minor changes

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@
 
 
 $(document).ready(function () {
-   setTimeout(function(){
-      location.href="./pages/menuBar.html";
-   },2000);
-   
+   setTimeout(function () {
+      location.href = "./pages/menuBar.html";
+   }, 2000);
+
 });
